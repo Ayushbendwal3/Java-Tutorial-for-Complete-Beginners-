bdcdf13ae56b5e7012050b58aac9f81871a17d74
Ayush Bendwal <bendwalayush3@gmail.com>
2021-06-07 21:16:13 +0530

    String and text

---
+++ Tutorial3/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) {
+        int myInt = 7;
+
+        String text = "Hello";
+
+        String blank = " ";
+
+        String name = "Bob";
+
+        String greeting = text + blank + name;
+
+        double myDouble = 7.8;
+
+        System.out.println(greeting);
+
+        System.out.println("My integer is: " + myInt);
+        System.out.println("My double is: " + myDouble);
+    }
+}
