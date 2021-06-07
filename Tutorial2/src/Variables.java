2d9e048c70da6df9c6d97f0b09f9977245114dd2
Ayush Bendwal <bendwalayush3@gmail.com>
2021-06-07 21:05:03 +0530

    Variables and Primitive DataTypes

---
+++ Tutorial2/src/Variables.java
@@ -0,0 +1,27 @@
+public class Variables {
+    public static void main(String[] args) throws Exception {
+        int myNumber = 88;
+        short myShort = 874;
+        long myLong = 646213;
+
+        double myDouble = 7.3245;
+        float myFloat = 231.4f;
+
+        char myChar = 'u';
+
+        boolean myBoolean = true;
+
+        byte myByte = 127;
+
+        System.out.println("##################################");
+        System.out.println(myShort);
+        System.out.println(myNumber);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+        System.out.println("##################################");
+    }
+}
