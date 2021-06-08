19d0441aab92d4908e8fb83d40953dd7a378be95
Ayush Bendwal <bendwalayush3@gmail.com>
2021-06-08 10:19:16 +0530

    For Loop

---
+++ Tutorial5/src/ForLoop.java
@@ -0,0 +1,14 @@
+public class ForLoop {
+  public static void main(String[] args) {
+
+    // Infinite loop
+    // for (;;) {
+    // System.out.println("Infinite Loop");
+    // }
+
+    // Traditional For loop
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i);
+    }
+  }
+}
