47a1498f313ef699b5241950899d88c412ad3131
Ayush Bendwal <bendwalayush3@gmail.com>
2021-06-15 12:45:31 +0530

    To String

---
+++ Tutorial16/src/ToString.java
@@ -0,0 +1,13 @@
+public class ToString {
+  public static void main(String[] args) {
+    Frog a = new Frog(1, "Moisce");
+    Frog b = new Frog(2, "Moiscets");
+    Frog c = new Frog(3, "Moissdrce");
+    Frog d = new Frog(4, "Moisygce");
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+  }
+}
